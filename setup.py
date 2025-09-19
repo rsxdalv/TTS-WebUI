78faba15d44ad9c01a67a79bb9f11850bcb4596b
Roberts Slisans <rsxdalv@gmail.com>
2025-09-20 00:49:40 +0300

    update to pre-pypi distribution

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import setuptools
+from pathlib import Path
 
 # Define versions
 TORCH_VERSION = "2.7.0"
 CUDA_VERSION = "cu128"
 torch = f"torch=={TORCH_VERSION}"
 
-with open("requirements.txt") as f:
+HERE = Path(__file__).parent
+with open(HERE / "requirements.txt") as f:
     requirements = f.read().splitlines()
 
 # Define optional dependencies
@@ -16,14 +18,14 @@ extras_require = {
         "torchaudio",
     ],
     "cuda": [
-        # "xformers>=0.0.20",
-        # "triton>=2.0.0",
-        # "flash-attn>=2.0.0",
-        # f"torch=={TORCH_VERSION} --index-url https://download.pytorch.org/whl/{CUDA_VERSION}",
         f"{torch}+{CUDA_VERSION}",
+        # f"torch=={TORCH_VERSION} --index-url https://download.pytorch.org/whl/{CUDA_VERSION}",
         "torchvision",
         "torchaudio",
         "xformers",
+        # "xformers>=0.0.20",
+        # "triton>=2.0.0",
+        # "flash-attn>=2.0.0",
     ],
     "mac": [
         torch,
@@ -34,47 +36,43 @@ extras_require = {
         torch,
         "torchvision",
         "torchaudio",
-        # Add any ROCM specific packages here
     ],
     "intel": [
         torch,
         "torchvision",
         "torchaudio",
-        # --index-url https://download.pytorch.org/whl/test/xpu
     ],
 }
 
 setuptools.setup(
-    # name="tts_webui",
-    name="tts_webui_deps",
+    name="TTS-WebUI",
+    version="0.0.1",
+    # packages=[],
+    packages=["tts_webui"],
     # packages=setuptools.find_namespace_packages(),
-    # packages=["tts_webui"],
     # packages=setuptools.find_namespace_packages(
     #     include=[
     #         "tts_webui.*",
     #     ]
     # ),
     # include_package_data=True,
-    packages=[],
-    version="0.5.0",
+    package_data={"": ["*.json"]},
     author="rsxdalv",
     description="TTS WebUI / Harmonica",
+    long_description=open(HERE / "README.md", "r", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
     url="https://github.com/rsxdalv/tts-webui",
     project_urls={},
-    scripts=[],
-    # install_requires=[
-    #     "extension_kokoro @ git+https://github.com/rsxdalv/extension_kokoro@main",
-    #     "extension_rvc @ git+https://github.com/rsxdalv/extension_rvc@main",
-    #     "openai",
-    # ],
-    # install_requires=requirements,
-    # install_requires=[],
+    entry_points={
+        "console_scripts": [
+            "tts-webui=tts_webui.cli:main",
+        ],
+    },
     install_requires=requirements,
     extras_require=extras_require,
     dependency_links=[
         # "https://download.pytorch.org/whl/cu128",
     ],
-    package_data={"": ["*.json"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",

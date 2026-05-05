13f244c96bfef404d1c88c3a2819c575625389cf
Roberts Slisans <rsxdalv@gmail.com>
2026-05-05 13:50:05 +0300

    log react UI backend

--- react-ui/src/backend-utils/defaultBackend.tsx
+++ react-ui/src/backend-utils/defaultBackend.tsx
@@ -2,3 +2,7 @@ export const defaultBackend =
   process.env.GRADIO_BACKEND ||
   process.env.GRADIO_BACKEND_AUTOMATIC ||
   "http://127.0.0.1:7770/";
+
+console.log(
+  `Using Gradio backend: ${defaultBackend} (set GRADIO_BACKEND or GRADIO_BACKEND_AUTOMATIC environment variable to change)`
+)
